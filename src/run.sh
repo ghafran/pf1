ab7264720bf3e7f911566d037213a6a8d4583ae7
Ghafran Abbas <ghafranabbas@gmail.com>
2022-03-09 23:42:06 -0500

    a

--- src/run.sh
+++ src/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+nvidia-smi
+nvidia-smi --list-gpus | wc -l
+
 # attach database volume
 rm /src/RoseTTAFold/pdb100_2021Mar03
 rm /src/RoseTTAFold/UniRef30_2020_06
-rm /data/bfd//src/RoseTTAFold/bfd
+rm /data/bfd/src/RoseTTAFold/bfd
 ln -s /data/ /src/RoseTTAFold/pdb100_2021Mar03
 ln -s /data/ /src/RoseTTAFold/UniRef30_2020_06
-ln -s /data/bfd//src/RoseTTAFold/bfd 
+ln -s /data/bfd/src/RoseTTAFold/bfd 
 
 mkdir -p /src/RoseTTAFold/output
 cp /src/RoseTTAFold/example/input.fa /src/RoseTTAFold/output/input.fa
